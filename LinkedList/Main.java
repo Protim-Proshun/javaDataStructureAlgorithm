453540a675f3b0af38f59560837acfc82040e2bf
Protim-Proshun <protim.proshun@gmail.com>
2024-05-26 16:19:05 -0400

    Add LinkedList package with LinkedList class having nested class Node

---
+++ LinkedList/Main.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(5);
+        myLinkedList.printList();
+    }
+
+}
